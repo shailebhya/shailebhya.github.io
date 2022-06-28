fcaba9d6d4eac3cc51fa859513f799c2049a5544
shailebhya <shailebhya@gmail.com>
2022-06-28 08:48:11 +0530

    updt

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3600003ee9a64733a42f30f26d5d200b",
-"index.html": "39c6fef317103d164df467d402cac3dc",
-"/": "39c6fef317103d164df467d402cac3dc",
-"main.dart.js": "8d6309e02c27550a8680b1d5417e065c",
+"index.html": "f91181024e66ddbea6e444c0b72e9fa4",
+"/": "f91181024e66ddbea6e444c0b72e9fa4",
+"main.dart.js": "96ccdbfdd6fca436b8e16c4989fca87b",
 "404.html": "0a27a4163254fc8fce870c8cc3a3f94f",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
